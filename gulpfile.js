22e15d6c30406b99146a8423cca3e55bddb62641
radin-reth <radin.reth@gmail.com>
2017-06-19 11:23:47 +0700

    #node

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,12 @@ const gulp = require('gulp')
 gulp.task('default', () => {
   console.log('hello')
 })
+
+
+/*
+perform repeated task
+1. compile sass, jade
+2. minify file
+3. copy files
+4. ... 
+*/
